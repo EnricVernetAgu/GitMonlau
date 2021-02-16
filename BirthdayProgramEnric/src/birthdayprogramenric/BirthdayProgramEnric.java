73203ec7fb68406700532c9095b680c7d0618a3c
ENRIC VERNET AGUILÓ <enricveragu@monlau.com>
2021-02-16 09:22:51 +0100

    first commit

---
+++ BirthdayProgramEnric/src/birthdayprogramenric/BirthdayProgramEnric.java
@@ -0,0 +1,25 @@
+/*
+AUTOR: ENRIC VERNET AGUILO MONLAU
+FECHA: 16/02/2021
+*/
+package birthdayprogramenric;
+
+
+public class BirthdayProgramEnric {
+
+    public static void main(String[] args) {
+        int day=13, month=9, year=2021;
+        String stDat=birthDay_ZellerMonlau(day, month, year);
+        System.out.println("International Programmer's Day:"+stDat + "sep2021");
+    }
+    private static String birthDay_ZellerMonlau(int day, int month, int year){
+        String[] ArrayWeek = {"Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"};
+       int a, y, m, d;
+       a = (14 - month) / 12;
+       y = year -a;
+       m = month + 12 * a - 2;
+       d = (day + y + y / 4 - y / 100 + y / 400 + (31*m) / 12) % 7;
+       return ArrayWeek[d];
+    }
+    
+}
